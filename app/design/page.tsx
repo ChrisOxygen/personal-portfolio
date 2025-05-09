de1927e1736bc03833f7ee0abd69ccea8cb24c0c
Christopher Okafor <chrisoxygen1234@gmail.com>
2025-05-09 08:23:51 +0100

    add a coming soon screen in production

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -10,7 +10,7 @@ function DesignSystem() {
     <main className="bg-site-dark-purple-001 min-h-screen p-10 text-site-white">
       <Button className="cursor-pointer shadow-[12.61px_7.42px_7.71px_0px_rgba(23,28,37,0.24)] font-grotesk font-medium text-[20px] transition-all duration-1000 ease-in-out leading-none tracking-[-2%] bg-[linear-gradient(to_right,#8B63C2_0%,#8B63C2_75%,#BB8EF9_100%)] rounded-[30.77px] group relative inline-flex pl-[39px] pr-[27px] items-center justify-center overflow-hidden py-5 text-neutral-200 hover:scale-105">
         <div className="flex items-center gap-5">
-          <span className="">Let's talk</span>
+          <span className="">Let&apos;s talk</span>
           <Image
             src="/assets/button-arrow.svg"
             alt="Arrow right icon"
